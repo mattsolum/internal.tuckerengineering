eef94e89fff6cc95f71da7e3518eda5458edc7de
Matthew Solum <saven.matt@gmail.com>
2012-08-08 17:53:12 -0500

    Refactoring so that property and location are the same object

--- system/application/helpers/structures/client_helper.php
+++ system/application/helpers/structures/client_helper.php
@@ -1,22 +1,16 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed');
 
 
-class Client
+class StructClient
 {
-	//CI Base Class
-	private $base = NULL;
 	//Client Information
 	public $id = NULL;
 	public $name = '';
 	//Address
-	public $street_1 = '';
-	public $street_2 = '';
-	public $city = '';
-	public $state = '';
-	public $zip = '';
+	public $location = NULL;
 	//Contact Information
 	public $contact = array();
-	//Client this client represents
+	//Client that *this* client represents
 	public $parent = NULL;
 	//Notes
 	public $note = '';
