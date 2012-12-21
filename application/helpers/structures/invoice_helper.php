b9a574ab68bca6340a106a06ad11fb826c460bf2
Matthew Solum <saven.matt@gmail.com>
2012-12-21 16:44:06 -0600

    Invoices written, but untested.

--- application/helpers/structures/invoice_helper.php
+++ application/helpers/structures/invoice_helper.php
@@ -1,1 +1,132 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+class StructInvoice
+{
+	public $id 			= NULL;
+
+	public $client		= NULL;
+	public $jobs		= NULL;
+
+	public function construct($json = NULL)
+	{
+		$this->jobs 	= array();
+		$this->client 	= new StructClient();
+
+		if($json != NULL)
+		{
+			$this->set_from_json($json);
+		}
+	}
+
+	/**
+	 * Loads data from a json dump of this object
+	 * accepts a raw string of json or an interpreted object
+	 *
+	 * @author Matthew Solum
+	 * @param string/object $json
+	 */
+	public function set_from_json($json)
+	{
+		if(is_string($json))
+		{
+			$json = json_decode($json);
+		}
+		
+		if($json != NULL)
+		{	
+			if(isset($json->client))
+			{
+				$this->client->set_from_json($json->client);
+			}
+
+			if(isset($json->jobs))
+			{
+				foreach ($json->jobs as $job)
+				{
+					$this->jobs[] = new StructJob($job);
+				}
+			}
+		}
+	}
+
+	public function is_valid()
+	{
+		if(!$this->client->is_valid())
+		{
+			return FALSE;
+		}
+
+		if(count($this->jobs) == 0)
+		{
+			return FALSE;
+		}
+
+		foreach($this->jobs as $job)
+		{
+			if(!$job->is_valid())
+			{
+				return FALSE;
+			}
+		}
+
+		return FALSE;
+	}
+
+	public function sort_jobs()
+	{
+		$this->jobs = $this->quicksort_by_property($this->jobs, 'id');
+	}
+	
+	private function quicksort_by_property($arr, $property, $left = 0, $right = NULL)
+	{
+		// when the call is recursive we need to change
+		//the array passed to the function yearlier
+		static $array = array();
+		if( $right == NULL )
+		{
+			$array = $arr;
+			$right = count($array)-1;//last element of the array
+		}
+		 
+		$i = $left;
+		$j = $right;
+		 
+		$tmp = $array[(int)(($left+$right)/2)]->$property;
+		 
+		// partion the array in two parts.
+		// left from $tmp are with smaller values,
+		// right from $tmp are with bigger ones
+		do
+		{
+			while( $array[$i]->$property < $tmp )
+			$i++;
+			 
+			while( $tmp < $array[$j]->$property )
+			$j--;
+			 
+			// swap elements from the two sides
+			if( $i <= $j )
+			{
+				$w = $array[$i];
+				$array[$i] = $array[$j];
+				$array[$j] = $w;
+				 
+				$i++;
+				$j--;
+			}
+		}while( $i <= $j );
+		 
+		// devide left side if it is longer the 1 element
+		if( $left < $j )
+		$this->quicksort_by_property(NULL, $property, $left, $j);
+		 
+		// the same with the right side
+		if( $i < $right )
+		$this->quicksort_by_property(NULL, $property, $i, $right);
+		 
+		// when all partitions have one element
+		// the array is sorted
+		 
+		return $array;
+	}
+}
--- application/models/Invoice.php
+++ application/models/Invoice.php
@@ -10,57 +10,162 @@ class Invoice extends CI_Model {
 		$this->CI =& get_instance();
 		
 		$this->CI->load->model('Job');
+		$this->CI->load->model('Client');
 	}
-	
+	}
+
 	/**
 	 * Loads an invoice and supporting information into an invoice class
 	 *
 	 * @author Matthew Solum
 	 * @param $invoice_id
-	 * @return Invoice
+	 * @return StructInvoice
 	 */
-	public function get($invoice_id)
+	public function get($client_id, $invoice_id)
 	{
-		//code
-		$invoice = $this->query();
+		$invoice 			= new StructInvoice();
+
+		$invoice->id 		= $invoice_id;
+		$invoice->client 	= $this->CI->Client->get($client_id);
+
+		$where = array('client_id' => $client_id, 'invoice_id' => $invoice_id);
+		$query = $this->CI->db->get_where('invoices', $where);
+
+		if($query->num_rows() > 0)
+		{
+			foreach ($query->result() as $row)
+			{
+				$invoice->jobs[] = $this->CI->Job->get($row->job_id);
+			}
+		}
+
+		return $invoice;
 	}
-	
-	
-	
+
 	/**
-	 * Creates a new invoice
+	 * Creates or updates an invoice
+	 * @param  StructInvoice $invoice
+	 * @return BOOL FALSE on failure, Int on success
+	 */
+	public function commit($invoice)
+	{
+		if(get_class($invoice) != 'StructInvoice' || !is_valid($invoice))
+		{
+			log_message('error', 'Error in Invoice method commit: invoice provided is invalid.')
+			return FALSE;
+		}
+
+		$id = $this->exists($invoice);
+		if($id !== FALSE)
+		{
+			//The exists function checks to see if
+			//an invoice exists for the same client
+			//with the same list of jobs.
+			//If it does, may as well call it quits,
+			//that's all this model handles.
+			return $id;
+		}
+		else
+		{
+			return $this->create($invoice);
+		}
+	}
+
+	/**
+	 * Creates a new invoice.
+	 * Returns the invoice ID on success
 	 *
 	 * @author Matthew Solum
-	 * @param $invoice		
+	 * @param StructInvoice $invoice		
+	 * @return BOOL FALSE on failure, Int on success
+	 */
+	private function create($invoice)
+	{
+		$this->CI->db->trans_start();
+
+		$id = $this->get_next_invoice_id($invoice->client->id);
+
+		foreach($invoice->jobs AS $job)
+		{
+			if(!$this->create_single($id, $invoice->client->id, $job->id))
+			{
+				break;
+			}
+		}
+
+		$this->CI->db->trans_complete();
+		
+		if($this->CI->db->trans_status() === FALSE)
+		{
+			log_message('Error', 'Error in Invoice method create: transaction failed.');
+			return FALSE;
+		}
+		else
+		{
+			return TRUE;
+		}
+	}
+	
+	/**
+	 * Inserts a single record for an invoice
+	 * 
+	 * @param  int $invoice_id
+	 * @param  int $client_id 
+	 * @param  int $job_id
 	 * @return BOOL
 	 */
-	public function create($invoice)
+	private function create_single($invoice_id, $client_id, $job_id)
 	{
-		//code
+		$this->CI->db->trans_start();
+
+		$data = array();
+		$data['invoice_id'] = $invoice_id;
+		$data['client_id']	= $client_id;
+		$data['job_id']		= $job_id;
+		$data['date_added']	= now();
+
+		$this->CI->db->insert('invoices', $data);
+
+		$this->CI->db->trans_complete();
+		
+		if($this->CI->db->trans_status() === FALSE)
+		{
+			log_message('Error', 'Error in Invoice method create: transaction failed.');
+			return FALSE;
+		}
+		else
+		{
+			return TRUE;
+		}
 	}
 	
 	/**
-	 * Updates an invoice
-	 *
-	 * @author Matthew Solum
-	 * @param $invoice
-	 * @return bool
-	 */
-	public function update($invoice)
-	{
-		//code
-	}
-	
-	/**
-	 * Deletes an invoice given a invoice_id
-	 *
-	 * @author Matthew Solum
-	 * @param $invoice_id
-	 * @return bool
+	 * Deletes an invoice given a client and invoice ID
+	 * @param  int $client_id 
+	 * @param  int $invoice_id
+	 * @return BOOL
 	 */
 	public function delete($client_id, $invoice_id)
 	{
-		//code
+		$this->CI->db->trans_start();
+
+		$where = array();
+		$where['client_id'] 	= $client_id;
+		$where['invoice_id']	= $invoice_id;
+
+		$this->CI->db->delete('invoices', $where);
+
+		$this->CI->db->trans_complete();
+		
+		if($this->CI->db->trans_status() === FALSE)
+		{
+			log_message('Error', 'Error in Invoice method create: transaction failed.');
+			return FALSE;
+		}
+		else
+		{
+			return TRUE;
+		}
 	}
 	
 	/**
@@ -70,8 +175,60 @@ class Invoice extends CI_Model {
 	 * @param $invoice
 	 * @return bool
 	 */
-	public function exists($invoice)
+	private function exists($invoice)
 	{
-		//code
+		//Assemble a sorted list of job ID's separated by spaces
+		//into one string to be compared to a similarly constructed string
+		//from a MySQL query.
+		$jobs_str = '';
+
+		$invoices->sort_jobs();
+		foreach ($invoice->jobs as $job) {
+			$jobs_str .= $job->id . ' ';
+		}
+
+		$jobs_str = substr($jobs_str, 0, strlen(jobs_str) - 1);
+
+		$query = $this->CI->db->query("SELECT invoice_id, client_id, GROUP_CONCAT(job_id ORDER BY job_id ASC SEPARATOR ' ') AS jobs FROM invoices WHERE client_id = " . $invoice->client->id . " GROUP BY invoice_id, client_id");
+
+		if($query->num_rows() > 0)
+		{
+			//MySQL was not allowing me to compare to the GROUP_CONCAT string
+			//so I am sorting through the invoices that belong to the given
+			//client manually
+			foreach ($query->result() as $row)
+			{
+				if($row->jobs == $jobs_str)
+				{
+					return $row->invoice_id;
+				}
+			}
+		}
+
+		return FALSE;
+	}
+
+	/**
+	 * Gets the next invoice ID given a client ID
+	 * @param  Int $client_id
+	 * @return Int
+	 */
+	private function get_next_invoice_id($client_id)
+	{
+		$this->CI->db->select('invoice_id');
+		$this->CI->db->from('invoices');
+		$this->CI->db->order_by('invoice_id', 'DESC');
+		$this->CI->db->limit(1);
+
+		$query = $this->CI->db->get();
+
+		if($query->num_rows() > 0)
+		{
+			$row = $query->row(0);
+
+			return $row->invoice_id + 1;
+		}
+
+		return 0;
 	}
 }
